90d3a3ddaf0d96167a6bb715588f76838a5da9fe
Sam Wallan <44255917+swallan@users.noreply.github.com>
2021-07-26 22:00:53 -0700

    mmf

--- run_paralell.sh
+++ run_paralell.sh
@@ -2,24 +2,26 @@
 
 trap 'kill $(jobs -p)' SIGINT
 
-cd qmc_experiment &
-conda activate scipydev &
-python3 generate_qmc_combinations_and_mpmath.py 0 625 &
-python3 generate_qmc_combinations_and_mpmath.py 625 1250 &
-python3 generate_qmc_combinations_and_mpmath.py 1250 1875 &
-python3 generate_qmc_combinations_and_mpmath.py 1875 2500 &
-python3 generate_qmc_combinations_and_mpmath.py 2500 3125 &
-python3 generate_qmc_combinations_and_mpmath.py 3125 3750 &
-python3 generate_qmc_combinations_and_mpmath.py 3750 4375 &
-python3 generate_qmc_combinations_and_mpmath.py 4375 5000 &
-python3 generate_qmc_combinations_and_mpmath.py 5000 5625 &
-python3 generate_qmc_combinations_and_mpmath.py 5625 6250 &
-python3 generate_qmc_combinations_and_mpmath.py 6250 6875 &
-python3 generate_qmc_combinations_and_mpmath.py 6875 7500 &
-python3 generate_qmc_combinations_and_mpmath.py 7500 8125 &
-python3 generate_qmc_combinations_and_mpmath.py 8125 8750 &
-python3 generate_qmc_combinations_and_mpmath.py 8750 9375 &
-python3 generate_qmc_combinations_and_mpmath.py 9375 10000 &
+python3 generate_qmc_combinations_and_mpmath.py 0 5 &
+python3 generate_qmc_combinations_and_mpmath.py 5 10 &
+python3 generate_qmc_combinations_and_mpmath.py 10 15 &
+python3 generate_qmc_combinations_and_mpmath.py 15 20 &
+python3 generate_qmc_combinations_and_mpmath.py 20 25 &
+python3 generate_qmc_combinations_and_mpmath.py 25 30 &
+python3 generate_qmc_combinations_and_mpmath.py 30 35 &
+python3 generate_qmc_combinations_and_mpmath.py 35 40 &
+python3 generate_qmc_combinations_and_mpmath.py 40 45 &
+python3 generate_qmc_combinations_and_mpmath.py 45 50 &
+python3 generate_qmc_combinations_and_mpmath.py 50 55 &
+python3 generate_qmc_combinations_and_mpmath.py 55 60 &
+python3 generate_qmc_combinations_and_mpmath.py 60 65 &
+python3 generate_qmc_combinations_and_mpmath.py 65 70 &
+python3 generate_qmc_combinations_and_mpmath.py 70 75 &
+python3 generate_qmc_combinations_and_mpmath.py 75 80 &
+python3 generate_qmc_combinations_and_mpmath.py 80 85 &
+python3 generate_qmc_combinations_and_mpmath.py 85 90 &
+python3 generate_qmc_combinations_and_mpmath.py 90 95 &
+python3 generate_qmc_combinations_and_mpmath.py 95 100 &
 
 wait
 
